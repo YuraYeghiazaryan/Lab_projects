b1214bb51e6053f5f4f9570639e0da94b6572daa
YuraYeghiazaryan <101919335+YuraYeghiazaryan@users.noreply.github.com>
2022-10-13 00:18:33 +0400

    Create exe.py

---
+++ exe.py
@@ -0,0 +1,19 @@
+from config import *
+import pygame as pg
+from chess_items import *
+
+
+clock = pg.time.Clock()
+screen = pg.display.set_mode(win_size)
+pg.display.set_caption("Pair to pair Chess")
+screen.fill((150, 100, 25))
+
+chessboard = Chessboard(screen)
+
+run = True
+while run:
+    clock.tick(FPS)
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            run = False
