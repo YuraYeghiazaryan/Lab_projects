701ecbbaa0c45fa80d7368f23c5db5bd5d316bdc
YuraYeghiazaryan <101919335+YuraYeghiazaryan@users.noreply.github.com>
2022-10-14 08:53:02 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,18 @@
 win_size = (1000, 700)
 FPS = 10
 cells = 8
-size_cell = 70
-colors = [(239, 219, 197), (130, 69, 19)]
+cell_size = 57
+COLORS = [(239, 219, 197), (130, 69, 19)]
+COLORS_FOR_BOARD = ['images/white_for_board.png', 'images/black_for_board.png']
 fnt_size = 18
-img_path = 'images/back.jpeg'
+IMG_PATH = 'images/back.jpeg'
+LTRS = 'ABSDEFGH'
+PIECES_TYPES = {
+    'k': ('King', 'b'), 'K': ('King', 'w'),
+    'q': ('Queen', 'b'), 'Q': ('Queen', 'w'),
+    'r': ('Rook', 'b'), 'R': ('Rook', 'w'),
+    'b': ('Bishop', 'b'), 'B': ('Bishop', 'w'),
+    'n': ('Knight', 'b'), 'N': ('Knight', 'w'),
+    'p': ('Pawn', 'b'), 'P': ('Pawn', 'w')
+}
+PIECE_PATH = 'images/pieces/'
